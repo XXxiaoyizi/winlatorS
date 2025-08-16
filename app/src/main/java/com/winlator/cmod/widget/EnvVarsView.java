e9b81a725843b7cab6560686738ff86e511b48a2
pipetto-crypto <pipetto-crypto@gmail.com>
2025-08-16 18:42:11 +0200

    EnvVarsView: add MESA_GL_VERSION_OVERRIDE env var

--- app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
+++ app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
@@ -36,6 +36,7 @@ public class EnvVarsView extends FrameLayout {
         {"TU_DEBUG", "SELECT_MULTIPLE", "startup", "nir", "nobin", "sysmem", "gmem", "forcebin", "layout", "noubwc", "nomultipos", "nolrz", "nolrzfc", "perf", "perfc", "flushall", "syncdraw", "push_consts_per_stage", "rast_order", "unaligned_store", "log_skip_gmem_ops", "dynamic", "bos", "3d_load", "fdm", "noconform", "rd"},
         {"DXVK_HUD", "SELECT_MULTIPLE", "devinfo", "fps", "frametimes", "submissions", "drawcalls", "pipelines", "descriptors", "memory", "gpuload", "version", "api", "cs", "compiler", "samplers"},
         {"MESA_EXTENSION_MAX_YEAR", "NUMBER"},
+        {"MESA_GL_VERSION_OVERRIDE", "NUMBER"},
         {"PULSE_LATENCY_MSEC", "NUMBER"},
         {"WINE_DO_NOT_CREATE_DXGI_DEVICE_MANAGER", "CHECKBOX", "0", "1"},
         {"WINE_NEW_MEDIASOURCE", "CHECKBOX", "0", "1"},
